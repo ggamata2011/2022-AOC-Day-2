f5a44ac921c4e51236c7a9e98be340c744440a54
ggamata2011 <37355821+ggamata2011@users.noreply.github.com>
2022-12-11 02:11:51 -0800

    Added HTML page of of puzzle
    Included starter files for code

---
+++ AOC Day 2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+//Use for file input and output
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+
+int main()
+{
+//Input filestream
+ifstream file;
+
+file.open("input.txt");
+
+while(!file.eof()){
+
+
+
+}
+
+}
+
+
+
+
+
